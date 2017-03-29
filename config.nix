2e99f5b2ef1ab07a8c524bdd256e8b81e49a5f59
reckbo <reckbo@bwh.harvard.edu>
2017-03-29 22:14:03 +0000

    config.nix

--- config.nix
+++ config.nix
@@ -7,8 +7,11 @@
   {
     rEnv = super.rWrapper.override {
       packages = with self.rPackages; [
-      data_table ggplot2 dplyr magrittr xtable lubridate rjags
-      foreach lintr memoise RSQLite devtools
+      data_table magrittr xtable
+      foreach lintr memoise RSQLite devtools rstan rjags
+      # tidyverse
+      purrr dplyr modelr stringr lubridate ggplot2 tibble
+      stringr hms tidyr readr
       ];
     };
   };
